dbefda59fccc5a217a13c14a28357137c8160e36
Peter Cook <peter@peterrcook.com>
2020-01-17 13:40:33 +0000

    Start converting to particle-flow.js library

---
+++ particle-flow.js
@@ -0,0 +1,180 @@
+function ParticleFlow(config) {
+    this.canvas = config.canvasEl;
+    this.ctx = null;
+    this.w = null;
+    this.h = null;
+    this.seeds = [];
+    this.numParticles = 0;
+    this.speedFactor = 1;
+
+    
+    this.field = null;
+    this.particles = [];
+    this.prevT = null;
+    this.isRunning = true;
+
+    this._init();
+}
+
+
+// Private
+ParticleFlow.prototype._init = function() {
+    this.w = this.canvas.clientWidth;
+    this.h = this.canvas.clientHeight;
+
+    this.ctx = this.canvas.getContext('2d');
+}
+
+
+// Utility
+ParticleFlow.prototype._distanceBetweenPts = function(x0, y0, x1, y1) {
+    var dx = x0 - x1;
+    var dy = y0 - y1;
+    return Math.sqrt(dx * dx + dy * dy);
+}
+
+ParticleFlow.prototype._getRandomPoint = function(w, h) {
+    return {
+        x: Math.floor(Math.random() * w),
+        y: Math.floor(Math.random() * h)
+    }
+}
+
+
+// Field functions
+ParticleFlow.prototype._updateField = function() {
+    console.time('updateField');
+
+    this.field = [];
+    var numSeeds = this.seeds.length;
+
+    for(var x = 0; x < this.w; x++) {
+        this.field.push([]);
+
+        for(var y = 0; y < this.h; y++) {
+            this.field[x].push([0, 0]);  // pushed array is [vx, vy]
+
+            // Loop through seeds
+            for(var i = 0; i < numSeeds; i++) {
+                var seed = this.seeds[i];
+
+                // Compute distance from seed to [x,y]
+                var dis = this._distanceBetweenPts(seed.x, seed.y, x, y);
+
+                // Skip this seed if point is outside seed's radius of influence
+                if(dis > seed.r) {
+                    continue;
+                }
+
+                // seed.vx and seed.vy are pixels per second. Divide by 1000 so that field unit is pixels per millisecond.
+                this.field[x][y][0] += seed.strength(dis) * seed.vx / 1000;
+                this.field[x][y][1] += seed.strength(dis) * seed.vy / 1000;
+            }
+        }
+    }
+
+    console.timeEnd('updateField');
+}
+
+
+// Particle functions
+ParticleFlow.prototype._initParticles = function() {
+    this.particles = [];
+
+    for(var i = 0; i < this.numParticles; i++) {
+        var pt = this._getRandomPoint(this.w, this.h);
+        this.particles.push({
+            prevX: pt.x,
+            prevY: pt.y,
+            x: pt.x,
+            y: pt.y
+        });  
+    }
+}
+
+ParticleFlow.prototype._updateParticles = function(dt) {
+    // console.time('updateParticles');
+
+    for(var i = 0; i < this.numParticles; i++) {
+        var p = this.particles[i];
+
+        p.prevX = p.x;
+        p.prevY = p.y;
+
+        var f = this.field[Math.floor(p.x)][Math.floor(p.y)];
+        p.x = p.x + dt * f[0];
+        p.y = p.y + dt * f[1];
+
+        var isZeroVelocity = f[0] === 0 && f[1] === 0;
+        var isOutOfBounds = p.x < 0 || p.x >= w || p.y < 0 || p.y >= h;
+        if(isZeroVelocity || isOutOfBounds) {
+            var pt = this._getRandomPoint(w, h);
+            p.x = p.prevX = pt.x;
+            p.y = p.prevY = pt.y;
+        }
+    }
+
+    // console.timeEnd('updateParticles');
+}
+
+
+
+// Rendering and animation
+ParticleFlow.prototype._drawParticles = function() {
+    // console.time('drawParticles');
+    var ctx = this.ctx;
+
+    ctx.save();
+    ctx.fillStyle = 'rgba(255, 255, 255, 0.03)';
+    ctx.fillRect(0, 0, this.w, this.h);
+    ctx.restore();
+
+    for(var i = 0; i < this.numParticles; i++) {
+        var p = this.particles[i];
+
+        ctx.beginPath();
+        ctx.moveTo(p.prevX, p.prevY);
+        ctx.lineTo(p.x, p.y);
+        ctx.stroke();
+    }
+    // console.timeEnd('drawParticles');
+}
+
+ParticleFlow.prototype._doAnimFrame = function(t) {
+    if(!this.prevT) {
+        this.prevT = t;
+    }
+
+    var deltaT = t - this.prevT;
+    deltaT *= this.speedFactor;
+
+    // drawSeeds();
+    this._updateParticles(deltaT);
+    this._drawParticles();
+
+    if(this.isRunning) {
+        window.requestAnimationFrame(this._doAnimFrame.bind(this));
+    }
+
+    this.prevT = t;
+}
+
+
+// Public
+ParticleFlow.prototype.setSeeds = function(seeds) {
+    this.seeds = seeds;
+    this._updateField();    
+}
+
+ParticleFlow.prototype.setNumberOfParticles = function(num) {
+    this.numParticles = num;
+    this._initParticles();
+}
+
+ParticleFlow.prototype.start = function() {
+    window.requestAnimationFrame(this._doAnimFrame.bind(this));
+}
+
+ParticleFlow.prototype.stop = function() {
+    this.isRunning = false;
+}
