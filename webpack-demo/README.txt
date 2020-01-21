65297ecb63909dc3a76a30a398565a122a4ab960
Peter Cook <peter@peterrcook.com>
2020-01-21 09:47:22 +0000

    Add webpack demo

---
+++ webpack-demo/README.txt
@@ -0,0 +1,19 @@
+# Particle flow webpack build
+
+This is an example webpack build.
+
+To run:
+
+```
+npm install
+
+npx webpack
+```
+
+Then start a webserver in this directory e.g.
+
+```
+http-server
+```
+
+Open your browser at the server's port.
---
+++ webpack-demo/src/index.js
@@ -0,0 +1,57 @@
+import ParticleFlow from 'particle-flow';
+
+var w = 800, h = 600;
+var seedRadius = 1000;
+var strengthFunction = function(dis) {
+    var s = 1 - dis / seedRadius;
+    return s * s;
+}
+
+var seeds = [
+    {
+        x: 0.15 * w,
+        y: 0.25 * h,
+        vx: 70, // velocities in pixels / sec
+        vy: 20,
+        r: seedRadius,
+        strength: strengthFunction
+    },
+    {
+        x: 0.75 * w,
+        y: 0.5 * h,
+        vx: 50,
+        vy: -100,
+        r: seedRadius,
+        strength: strengthFunction
+    },
+    {
+        x: 0.5 * w,
+        y: 0.75 * h,
+        vx: -100,
+        vy: 50,
+        r: seedRadius,
+        strength: strengthFunction
+    },
+    {
+        x: 0.25 * w,
+        y: 0.5 * h,
+        vx: 0,
+        vy: -30,
+        r: seedRadius,
+        strength: strengthFunction
+    },
+];
+
+var pf;
+var canvasEl = document.getElementById('canvas');
+
+function initParticleFlow() {
+    pf = new ParticleFlow(document.getElementById('canvas'));
+
+    pf.setSeeds(seeds);
+    pf.setNumberOfParticles(100);
+    pf.start();
+}
+
+initParticleFlow();
+
