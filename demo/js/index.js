e3866093fb179e0e9afd2c52e178df9f8df05b0b
Peter Cook <peter@peterrcook.com>
2020-01-25 10:07:34 +0000

    Change default seed radius to 300 in demo

--- demo/js/index.js
+++ demo/js/index.js
@@ -1,5 +1,5 @@
 var w = 800, h = 600;
-var seedRadius = 500;
+var seedRadius = 300;
 var strengthFunction = function(dis) {
     var s = 1 - dis / seedRadius;
     return s * s;
@@ -50,7 +50,7 @@ var datData = {
     particleColor: '#fff',
     speedFactor: 1,
     fadeFactor: 0.03,
-    seedRadius: 500,
+    seedRadius: 300,
     showSeeds: true
 }
 
